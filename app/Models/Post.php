d6420b2195bd9a17c5ad54685b0c0ac04806aebd
sail <sail@1034835b4168>
2023-05-16 20:36:11 +0000

    Bloggin post project

---
+++ app/Models/Post.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+       'title',
+       'slug',
+       'thumbnail',
+       'body',
+       'active',
+       'published_at',
+       'user_id',
+       'meta_data',
+       'meta_description'
+    ];
+
+
+    protected $casts = [
+
+        'published_at' => 'datetime'
+    ];
+
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The roles that belong to the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function shortBody(): string
+    {
+        return Str::words(strip_tags($this->body), 30);
+    }
+
+    public function getFormatedDate()
+    {
+        return $this->published_at->format('F jS Y');
+    }
+
+    public function getThumbnail()
+    {
+         if(str_starts_with($this->thumbnail, 'http'))
+         {
+            return $this->thumbnail;
+         }
+
+         return '/storage/' . $this->thumbnail;
+    }
+}
