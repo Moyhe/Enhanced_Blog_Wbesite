3d4d0ffddb9cba13128758c1ba4b3a176bf60f84
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-05 23:14:11 +0300

    configure deployment files

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -1,24 +1,23 @@
-
 <?php
 /** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
 ?>
 
-<x-app-layout  :meta-title="$category->title" meta-description="welcome to the story of love kanekiTouka">
+<x-app-layout :meta-title="$category->title" meta-description="welcome to the story of love kanekiTouka">
 
-<!-- Posts Section -->
-<div class="flex">
-    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+    <!-- Posts Section -->
+    <div class="flex">
+        <section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
-        @foreach ($posts as $post)
+            @foreach ($posts as $post)
 
-        <x-post-items :post="$post"></x-post-items>
+            <x-post-items :post="$post"></x-post-items>
 
-        @endforeach
-        {{ $posts->onEachSide(1)->links() }}
+            @endforeach
+            {{ $posts->onEachSide(1)->links() }}
 
-      </section>
+        </section>
 
-      <x-sidebar></x-sidebar>
-</div>
+        <x-sidebar></x-sidebar>
+    </div>
 
 </x-app-layout>
--- resources/views/post/search.blade.php
+++ resources/views/post/search.blade.php
@@ -5,24 +5,25 @@
         <section class="w-full md:w-2/3  px-3">
             <div class=" flex flex-col">
                 @foreach($posts as $post)
+                <div>
+                    <a href="{{route('view', $post)}}">
+                        <h2 class="text-blue-500 font-bold text-lg sm:text-xl mb-2">
+                            {!! str_replace(request()->get('q'), '<span
+                                class="bg-yellow-300">'.request()->get('q').'</span>', $post->title) !!}
+                        </h2>
+                    </a>
                     <div>
-                        <a href="{{route('view', $post)}}">
-                            <h2 class="text-blue-500 font-bold text-lg sm:text-xl mb-2">
-                                {!! str_replace(request()->get('q'), '<span class="bg-yellow-300">'.request()->get('q').'</span>', $post->title) !!}
-                            </h2>
-                        </a>
-                        <div>
-                            {{$post->shortBody()}}
-                        </div>
+                        {{$post->shortBody()}}
                     </div>
-                    <hr class="my-4">
+                </div>
+                <hr class="my-4">
                 @endforeach
             </div>
             {{ $posts->links() }}
         </section>
 
         <!-- Sidebar Section -->
-        <x-sidebar/>
+        <x-sidebar />
 
     </div>
 </x-app-layout>
